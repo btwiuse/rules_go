b8fd0bb7a7c384eca8c9c179754cbf6644e67feb
Btwiuse Arch <54848194+btwiuse@users.noreply.github.com>
2022-07-10 15:01:46 +0000

    fix build

--- go/tools/builders/asm_go1.go
+++
@@ -1,8 +0,0 @@
-//go:build !go1.19
-
-package main
-
-func asmAddPackagePathArg(args []string, packagePath string) []string {
-	// go tool asm does neither support nor require the -p arg in Go < 1.19.
-	return args
-}
